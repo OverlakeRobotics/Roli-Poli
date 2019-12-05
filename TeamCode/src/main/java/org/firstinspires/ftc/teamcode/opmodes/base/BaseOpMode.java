4fd777d6abbfaa634bf909a3819707c850d02ff7
thort <44458087+thort1@users.noreply.github.com>
2019-12-05 12:47:36 -0800

    Fixes null pointer exception.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/base/BaseOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/base/BaseOpMode.java
@@ -43,6 +43,8 @@ public abstract class BaseOpMode extends OpMode {
         driveSystem = new DriveSystem(driveMap, hardwareMap.get(BNO055IMU.class, "imu"));
 
         EnumMap<LatchSystem.Latch, Servo> latchMap = new EnumMap<>(LatchSystem.Latch.class);
+        latchMap.put(LatchSystem.Latch.LEFT, hardwareMap.get(Servo.class, "latchLeft"));
+        latchMap.put(LatchSystem.Latch.RIGHT, hardwareMap.get(Servo.class, "latchRight"));
         latchSystem = new LatchSystem(latchMap);
 
         lightSystem = new LightSystem(hardwareMap.get(DigitalChannel.class, "right_light"), hardwareMap.get(DigitalChannel.class, "left_light"));
